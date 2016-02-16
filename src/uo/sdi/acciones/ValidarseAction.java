0648fc3b7c1180b521776b6b57253be6ee439759
unknown <UO237124@EII-L-11-17.aulas.uniovi.es>
2016-02-16 10:29:59 +0100

    log de usuario

--- src/uo/sdi/acciones/ValidarseAction.java
+++ src/uo/sdi/acciones/ValidarseAction.java
@@ -14,37 +14,41 @@ public class ValidarseAction implements Accion {
 	@Override
 	public String execute(HttpServletRequest request,
 			HttpServletResponse response) {
-		
-		String resultado="EXITO";
-		String nombreUsuario=request.getParameter("nombreUsuario");
-		HttpSession session=request.getSession();
-		if (session.getAttribute("user")==null) {
+
+		String resultado = "EXITO";
+		String nombreUsuario = request.getParameter("nombreUsuario");
+		String contrasenya = request.getParameter("contrasenya");
+		HttpSession session = request.getSession();
+		if (session.getAttribute("user") == null) {
 			UserDao dao = PersistenceFactory.newUserDao();
 			User userByLogin = dao.findByLogin(nombreUsuario);
-			if (userByLogin!=null) {
+			if (userByLogin != null
+					&& userByLogin.getPassword().equals(contrasenya)) {
 				session.setAttribute("user", userByLogin);
-				int contador=Integer.parseInt((String)request.getServletContext().getAttribute("contador"));
-				request.getServletContext().setAttribute("contador", String.valueOf(contador+1));
-				Log.info("El usuario [%s] ha iniciado sesión",nombreUsuario);
-			}
-			else {
+				int contador = Integer.parseInt((String) request
+						.getServletContext().getAttribute("contador"));
+				request.getServletContext().setAttribute("contador",
+						String.valueOf(contador + 1));
+				Log.info("El usuario [%s] ha iniciado sesión", nombreUsuario);
+			} else {
 				session.invalidate();
-				Log.info("El usuario [%s] no está registrado",nombreUsuario);
-				resultado="FRACASO";
+				Log.info("El usuario [%s] no está registrado", nombreUsuario);
+				resultado = "FRACASO";
 			}
+		} else if (!nombreUsuario.equals(session.getAttribute("user"))) {
+			Log.info(
+					"Se ha intentado iniciar sesión como [%s] teniendo la sesión iniciada como [%s]",
+					nombreUsuario,
+					((User) session.getAttribute("user")).getLogin());
+			session.invalidate();
+			resultado = "FRACASO";
 		}
-		else
-			if (!nombreUsuario.equals(session.getAttribute("user"))) {
-				Log.info("Se ha intentado iniciar sesión como [%s] teniendo la sesión iniciada como [%s]",nombreUsuario,((User)session.getAttribute("user")).getLogin());
-				session.invalidate();
-				resultado="FRACASO";
-			}
 		return resultado;
 	}
-	
+
 	@Override
 	public String toString() {
 		return getClass().getName();
 	}
-	
+
 }
